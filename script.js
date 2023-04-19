a0a7bf93739298a23d983aa6c56aa0425bb7e8f5
alex <marquez798@gmail.com>
2023-04-18 23:56:58 -0500

    save to local storage

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $("document").ready(function(){
-  
   var today = dayjs();
-  $('#currentDay').text(today.format('MMM D, YYYY'));
+
+  $('#currentDay').text(today.format('[Today is:] MMM D, YYYY'));
+
+   $(".saveBtn").click(function(e){
+        //save the text and hour from the text field to local storage
+        //this is what we are targeting so the text area for the todos and then the hour of day. 
+        var toDO = $(this).parent().find("textarea")[0].value
+        var hourEl =  $(this).parent().find(".hour").text()
+        console.log(hourEl ,toDO)
+        localStorage.setItem(hourEl, toDO)
+      })
+
+    
+      $(function () {
   
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+     
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,16 +24,19 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
+
 });
 })

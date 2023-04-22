3b38151ba6907740edbe13d824a476778e365aa3
alex <marquez798@gmail.com>
2023-04-22 16:11:26 -0500

    ffixed color dynamic time

--- script.js
+++ script.js
@@ -1,42 +1,139 @@
-$("document").ready(function(){
-  var today = dayjs();
+  var currentTime = moment()
+  var timeContainer =$("#currentDay")
+  var formContainer = $("#formContainer")
+  var button =$("button")
 
-  $('#currentDay').text(today.format('[Today is:] MMM D, YYYY'));
+  function saveWork(event){
+    event.preventDefault()
+    if ($(this).attr("class") === "saveBtn 9"){
+      localStorage.setItem("block9", $("#block9").val());
+    }
+    else if ($(this).attr("class") === "saveBtn 10"){
+      localStorage.setItem("block10", $("#block10").val())
+    }
+    else if ($(this).attr("class") === "saveBtn 11"){
+      localStorage.setItem("block11", $("#block11").val())
+    }
+    else if ($(this).attr("class") === "saveBtn 12"){
+      localStorage.setItem("block12", $("#block12").val())
+    }
+    else if ($(this).attr("class") === "saveBtn 1"){
+      localStorage.setItem("block1", $("#block1").val())
+    }
+    else if ($(this).attr("class") === "saveBtn 2"){
+      localStorage.setItem("block2", $("#block2").val())
+    }
+    else if ($(this).attr("class") === "saveBtn 3"){
+      localStorage.setItem("block3", $("#block3").val())
+    }
+    else if ($(this).attr("class") === "saveBtn 4"){
+      localStorage.setItem("block4", $("#block4").val())
+    }
+    else if ($(this).attr("class") === "saveBtn 5"){
+      localStorage.setItem("block5", $("#block5").val())
+    } else {
+      localStorage.setItem("block5", $("#block5").val())
+    }
 
-   $(".saveBtn").click(function(e){
-        //save the text and hour from the text field to local storage
-        //this is what we are targeting so the text area for the todos and then the hour of day. 
-        var toDO = $(this).parent().find("textarea")[0].value
-        var hourEl =  $(this).parent().find(".hour").text()
-        console.log(hourEl ,toDO)
-        localStorage.setItem(hourEl, toDO)
-      })
+  }
 
-    
-      $(function () {
-  
-     
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  function loadPrev(){
+    $("#block9").val(localStorage.getItem("block9"))
+    $("#block10").val(localStorage.getItem("block10"))
+    $("#block11").val(localStorage.getItem("block11"))
+    $("#block12").val(localStorage.getItem("block12"))
+    $("#block1").val(localStorage.getItem("block1"))
+    $("#block2").val(localStorage.getItem("block2"))
+    $("#block3").val(localStorage.getItem("block3"))
+    $("#block4").val(localStorage.getItem("block4"))
+    $("#block5").val(localStorage.getItem("block5"))
+  }
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  function checkTime(){
+    var time9 = moment().hour(8);
+    var time10 = moment().hour(9)
+    var time11 = moment().hour(10)
+    var time12 = moment().hour(11)
+    var time1 = moment().hour(12)
+    var time2 = moment().hour(13)
+    var time3= moment().hour(14)
+    var time4 = moment().hour(15)
+    var time5 = moment().hour(16)
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+    if (time9 < currentTime) {
+      $(block9).addClass("past")
+  } else if (time9 > currentTime) {
+      $(block9).addClass("future")
+  } else {
+      $(block9).addClass("present")
+  }
+  if (time10 < currentTime) {
+      $(block10).addClass("past")
+  } else if (time10 > currentTime) {
+      $(block10).addClass("future")
+  } else {
+      $(block10).addClass("present")
+  }
+  if (time11 < currentTime) {
+      $(block11).addClass("past")
+  } else if (time11 > currentTime) {
+      $(block11).addClass("future")
+  } else {
+      $(block11).addClass("present")
+  }
+  if (time12 < currentTime) {
+      $(block12).addClass("past")
+  } else if (time12 > currentTime) {
+      $(block12).addClass("future")
+  } else {
+      $(block12).addClass("present")
+  }
+  if (time1 < currentTime) {
+      $(block1).addClass("past")
+  } else if (time1 > currentTime) {
+      $(block1).addClass("future")
+  } else {
+      $(block1).addClass("present")
+  }
+  if (time2 < currentTime) {
+      $(block2).addClass("past")
+  } else if (time2 > currentTime) {
+      $(block2).addClass("future")
+  } else {
+      $(block2).addClass("present")
+  }
+  if (time3 < currentTime) {
+      $(block3).addClass("past")
+  } else if (time3 > currentTime) {
+      $(block3).addClass("future")
+  } else {
+      $(block3).addClass("present")
+  }
+  if (time4 < currentTime) {
+      $(block4).addClass("past")
+  } else if (time4 > currentTime) {
+      $(block4).addClass("future")
+  } else {
+      $(block4).addClass("present")
+  }
+  if (time5 < currentTime) {
+      $(block5).addClass("past")
+  } else if (time5 > currentTime) {
+      $(block5).addClass("future")
+  } else {
+      $(block5).addClass("present")
+  }  
+  }
 
+  formContainer.on("click", ".saveBtn", saveWork)
 
-});
-})
+  function init(){
+    setInterval(function(){
+      timeContainer.text(currentTime.format("[current day:]MMMM Do [a] dddd, [time:] hh:mm A"))
+    }, 1000)
+    loadPrev()
+    checkTime()
+  }
+
+  init();
+
